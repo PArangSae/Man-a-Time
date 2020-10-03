c3d05856800b6ffb981f6e7b0391ff287f3e9c59
PArangSae_KoreanCoder_한국코더 <72248972+PArangSae@users.noreply.github.com>
2020-10-03 10:30:18 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+var d = new Date();
+print(d.getFullYear + "." + d.getMonth + "." + d.getDate + "  " + d.getHours + ":" + d.getMinutes + ":" + d.getSeconds)
+print("")
+print("")
+print("Man-a-Time, Manage your time")
+import("list.py")
