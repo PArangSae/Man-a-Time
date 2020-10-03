700e863810dadb7ef057649d46f4c6c896155908
PArangSae_KoreanCoder_한국코더 <72248972+PArangSae@users.noreply.github.com>
2020-10-03 11:30:20 +0900

    Create ATft.py

---
+++ ATft.py
@@ -0,0 +1,4 @@
+from datetime import datetime 
+if ftime.count(now.hour + "-" + now.minute + "-" now.second + "-") > 0 :
+  alf = ftime.index(now.hour + "-" + now.minute + "-" now.second + "-")
+  import "alf.js"
