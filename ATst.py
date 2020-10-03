c377092ca9bd344c6d923f663f3e06a96661eb1e
PArangSae_KoreanCoder_한국코더 <72248972+PArangSae@users.noreply.github.com>
2020-10-03 11:29:49 +0900

    Create ATst.py

---
+++ ATst.py
@@ -0,0 +1,4 @@
+from datetime import datetime 
+if stime.count(now.hour + "-" + now.minute + "-" now.second + "-") > 0 :
+  als = stime.index(now.hour + "-" + now.minute + "-" now.second + "-")
+  import "als.js"
